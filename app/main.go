b7949125013ebc794faf0c25a5d2af93c3b1ec66
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-07-08 19:10:15 +0100

    test:test the app

---
+++ app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, awgis!")
+	})
+
+	log.Println("✅ Target App running on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Fatal error: %s", err)
+	}
+}
